b6d884d09c0d768c063e28ca194ad216ca4f12b0
David Cauchi <david.cauchi@gmail.com>
2021-10-11 00:58:23 +0200

    Added tx signing and balance and amount checks

--- client/index.js
+++ client/index.js
@@ -1,4 +1,8 @@
 import "./index.scss";
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+
+const ec = new EC('secp256k1');
 
 const server = "http://localhost:3042";
 
@@ -16,19 +20,43 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+  let messageElement = document.getElementById("message");
+  messageElement.innerHTML = "";
+ 
+  let tx = getTransactionData();
+  let signature = signData(tx);
 
-  const body = JSON.stringify({
-    sender, amount, recipient
+  let body = JSON.stringify({
+    tx, signature
   });
-
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+    response.json().then(data =>{
+      if(response.ok) {
+        document.getElementById("balance").innerHTML = data.balance;
+        messageElement.innerHTML = data.message;
+      } else {
+        messageElement.innerHTML = data.err;
+      }
+    });
+  }).catch( error => {
+    console.log("Error: ", error);
   });
 });
+
+function getTransactionData(){
+  let sender = document.getElementById("exchange-address").value;
+  let amount = document.getElementById("send-amount").value;
+  let recipient = document.getElementById("recipient").value;
+
+  return {"sender":sender, "recipient":recipient, "amount":amount};
+}
+
+function signData(data){
+  let privateKey = document.getElementById("privateKey").value;
+  let key = ec.keyFromPrivate(privateKey);
+  let msgHash = SHA256(JSON.stringify(data));
+  // console.log(msgHash.toString());
+  return key.sign(msgHash.toString());
+}
--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const EC = require('elliptic').ec;
+const { SHA256 } = require('crypto-js');
+const ec = new EC('secp256k1')
 const port = 3042;
+const generatedAccounts = 3;
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const balances = {}
+
+for(let i = 0; i < generatedAccounts; i++){
+  const key = ec.genKeyPair();
+  const publicKey = key.getPublic().encode('hex');
+  const privateKey = key.getPrivate().toString(16);
+  balances[publicKey] = 100;
+  console.log({
+    privateKey: privateKey,
+    publicKey: publicKey,
+    balance: balances[publicKey]
+  });
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,10 +33,25 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {tx, signature} = req.body;
+  const {sender, recipient, amount} = tx;
+  const key = ec.keyFromPublic(sender, 'hex');
+  const msgHash = SHA256(JSON.stringify(tx)).toString();
+  // console.log(msgHash.toString());
+  if(!key.verify(msgHash, {r: signature.r, s: signature.s})){
+    res.status(401).send({ err:'Signature is incorrect.' });
+  } else {
+    if(amount <= 0 || isNaN(amount)){
+      res.status(402).send({ err:'Invalid amount: You can only transfer values greater than 0.' });
+    } else if(balances[sender] < amount){
+      const balance = balances[sender] || 0;
+      res.status(402).send({ err:`Insufficient funds: Your account balance is ${balance}. You requested to transfer ${amount}.` });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] = (balances[recipient] || 0) + +amount;
+      res.send({ balance: balances[sender], message: 'Transfer Successful.' });
+    }
+  }
 });
 
 app.listen(port, () => {
